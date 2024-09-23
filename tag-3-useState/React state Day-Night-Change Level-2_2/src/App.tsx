b794cbd083f9bac1c5991e0a59dbe01072fc5cb0
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-09-23 20:23:45 +0200

    fixed day/night text color

--- tag-3-useState/React state Day-Night-Change Level-2_2/src/App.tsx
+++ tag-3-useState/React state Day-Night-Change Level-2_2/src/App.tsx
@@ -6,9 +6,9 @@ const [isDarkMode, setIsDarkMode] = useState(false)
   return (
     <div>
   <h1 className={isDarkMode ? 
-    "mt-72 text-center text-black font-bold text-8xl" : 
-    "mt-72 text-center text-white font-bold text-8xl"}>
-      {isDarkMode ? "Day" : "Night"}
+    "mt-72 text-center text-white font-bold text-8xl" : 
+    "mt-72 text-center text-black font-bold text-8xl"}>
+      {isDarkMode ? "Night" : "Day"}
     </h1>
     <Switch isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
     </div>

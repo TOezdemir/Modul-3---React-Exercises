19566a10af0a147004d5705e7f2a6360a384b036
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-10-16 15:13:06 +0200

    minor design changes

--- tag-17-useRef/src/components/BirthdayForm.tsx
+++ tag-17-useRef/src/components/BirthdayForm.tsx
@@ -26,33 +26,37 @@ function BirthdayForm() {
     };
   
     return (
-<div className="font-sans flex flex-col items-center justify-center min-h-screen bg-gray-100">
-  <div className="bg-white p-6 rounded-lg shadow-md max-w-sm"> 
-    <h2 className="text-2xl font-semibold mb-4 text-center">Geburtstag prüfen</h2>
-    <div className="mb-4">
-      <label htmlFor="name" className="block text-gray-700 font-medium mb-2">Name:</label>
-      <input type="text" id="name"
-        ref={nameRef} 
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-      />
-    </div>
-    <div className="mb-6">
-      <label htmlFor="geburtstag"
-        className="block text-gray-700 font-medium mb-2">Geburtstag:</label>
-      <input type="date" id="geburtstag" 
-        ref={geburtstagRef} 
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-      />
-    </div>
-    <button 
-        onClick={geburtstagPruefen} 
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-    >
-      Prüfen 
-    </button>
-  </div>
-</div>
+        <div className="font-sans flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white p-6 rounded-lg shadow-md max-w-sm"> 
+                <h2 className="text-2xl font-semibold mb-4 text-center">Prüfe hier nach deinem Geburtstag!
+                <p className="text-sm mt-1">Alle die heute Geburtstag haben, haben besonders Glück, diese Seite gefunden zu haben!</p>
+                </h2>
+                <div className="mb-4">
+                <label htmlFor="name" className="block text-gray-700 font-medium mb-2">Name:</label>
+                <input type="text" id="name"
+                    ref={nameRef} 
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="geburtstag"
+                    className="block text-gray-700 font-medium mb-2">Geburtstag:</label>
+                    <input type="date" id="geburtstag" 
+                    ref={geburtstagRef} 
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                    />
+                </div>
+                <div className="mb-6 text-center">
+                <button 
+                onClick={geburtstagPruefen} 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                Prüfen 
+                </button>
+                </div>
+            </div>
+        </div>
     );
-  };
+};
   
-  export default BirthdayForm
+export default BirthdayForm
